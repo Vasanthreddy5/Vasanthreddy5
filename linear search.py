9f241e8fa26d070b3ebdb650380ebf318971d065
Alavalapalli Vasanth Reddy <alavalapalligithub@gmail.com>
2023-10-06 13:45:22 +0530

    updated linear search with comment of stash

--- linear search.py
+++ linear search.py
@@ -21,3 +21,8 @@ if result==-1:
     print("Element is not found in the array")
 else:
     print(f'Element is found at index:{result}')
+    
+'''normally if u keep your file in stash mode so to come out of stash we can use the command is git stash pop or git stash apply
+Git stash is a useful feature in git that allows you to temporarily save changes that you don't want to commit yet.it's like a "save point" for
+your work.you can use "git stash save" to stash your changes,and then use "git stash apply" or "git stash pop" to retrieve them later.it's great
+for switching branches or temporarily setting aside unfinished work'''
